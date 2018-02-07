babc82b90d820445195b4d8291cc415c1fd776ca
Christophe de Carvalho Pereira Martins <christophe.dcpm@gmail.com>
2018-02-07 13:29:35 +0100

    no need to set template_dir manually anymore
    since
    https://github.com/Jumpscale/0-robot/commit/99b3d68313c2047344ea06e69cb30961254d87bd
    template_dir is set automatically

--- templates/zerotier_client/zerotier_client_test.py
+++ templates/zerotier_client/zerotier_client_test.py
@@ -2,8 +2,8 @@ from unittest import TestCase
 from unittest.mock import MagicMock
 
 import pytest
-from js9 import j
 
+from js9 import j
 from zerotier_client import ZerotierClient
 
 
@@ -12,14 +12,12 @@ class TestZerotierClientTemplate(TestCase):
     def test_create_data_none(self):
         j.clients.zerotier.list = MagicMock(return_value=[])
 
-        ZerotierClient.template_dir = '/tmp'
         with pytest.raises(RuntimeError, message='template should fail to instanciate if data is None'):
             service = ZerotierClient(name="zttest", data=None)
 
     def test_create_data_no_token(self):
         j.clients.zerotier.list = MagicMock(return_value=[])
 
-        ZerotierClient.template_dir = '/tmp'
         with pytest.raises(RuntimeError, message="template should fail to instanciate if data doesn't contain 'token'"):
             service = ZerotierClient(name="zttest", data={'foo': 'bar'})
 
@@ -30,7 +28,6 @@ class TestZerotierClientTemplate(TestCase):
         j.clients.zerotier.list = MagicMock(return_value=[])
         j.clients.zerotier.get = MagicMock()
 
-        ZerotierClient.template_dir = '/tmp'
         data = {'token': 'foo'}
         service = ZerotierClient(name="zttest", data={'token': 'foo'})
         j.clients.zerotier.list.assert_called_with()
@@ -40,7 +37,6 @@ class TestZerotierClientTemplate(TestCase):
         j.clients.zerotier.list = MagicMock(return_value=['zttest'])
         j.clients.zerotier.get = MagicMock()
 
-        ZerotierClient.template_dir = '/tmp'
         data = {'token': 'foo'}
         service = ZerotierClient(name="zttest", data={'token': 'foo'})
         assert j.clients.zerotier.get.called is False
@@ -48,7 +44,6 @@ class TestZerotierClientTemplate(TestCase):
     def test_delete(self):
         j.clients.zerotier.delete = MagicMock()
 
-        ZerotierClient.template_dir = '/tmp'
         service = ZerotierClient(name="zttest", data={'token': 'foo'})
         service.delete()
         assert j.clients.zerotier.delete.called
