2cda1665beb85370487b5fc59465e687e4b2d014
john-kheir <kheirj@codescalers.com>
2018-04-05 14:10:25 +0200

    add container tests + req + prepare script + config

---
+++ tests/integration_tests/testsuite/a_basic/containers_tests.py
@@ -0,0 +1,53 @@
+import unittest
+from framework.zos_utils.utils import ZOS_BaseTest
+from random import randint
+
+
+class BasicTests(ZOS_BaseTest):
+    def __init__(self, *args, **kwargs):
+        super(BasicTests, self).__init__(*args, **kwargs)
+
+    @classmethod
+    def setUpClass(cls):
+        super(BasicTests, cls).setUpClass()
+
+    def setUp(self):
+        super(BasicTests, self).setUp()
+        self.temp_actions = {'node': {'actions': ['install']},
+                             'container': {'actions': ['install']}
+                             }
+
+    def test001_create_container(self):
+        """ ZRT-ZOS-001
+        *Test case for creatings container on a zero-os node*
+
+        **Test Scenario:**
+
+        #. Create a node service, should succeed.
+        #. Create a two container on that node, should succeed.
+        #. Check that the container have been created.
+        """
+        self.log('%s STARTED' % self._testID)
+
+        self.log('Create a two container on that node, should succeed.')
+        self.cont1_name = self.random_string()
+        self.containers = {self.cont1_name: {'node': self.zos_node_name,
+                                             'hostname': self.cont1_name,
+                                             'flist': self.cont_flist,
+                                             'storage': self.cont_storage}}
+
+        self.cont2_name = self.random_string()
+        self.containers = {self.cont2_name: {'node': self.zos_node_name,
+                                             'hostname': self.cont2_name,
+                                             'flist': self.cont_flist,
+                                             'storage': self.cont_storage}}
+
+        self.create_container(containers=self.containers, temp_actions=self.temp_actions)
+        self.wait_for_service_action_status(self.cont1_name, res[self.cont1_name]['install'])
+        self.wait_for_service_action_status(self.cont2_name, res[self.cont2_name]['install'])
+
+        self.log('Check that the container have been created.')
+        self.assertEqual(self.zos_client.containers.list(), 3)
+        ## make sure they exist by name and the params are reflected correctly
+
+        self.log('%s ENDED' % self._testID)
