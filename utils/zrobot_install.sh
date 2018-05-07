3dcb523fcb8a1b671e246933aa3b816b6cf6b194
Sarah Boctor <boctor.sarah@gmail.com>
2018-05-07 10:31:40 +0200

    Add missing clone to install script

--- utils/zrobot_install.sh
+++ utils/zrobot_install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -e
 
-mkdir -p /opt/code/github/zero-os
-sudo chown -R $USER:$USER /opt/code/github/zero-os
-cp -r . /opt/code/github/zero-os/0-robot
+# settings
+export BRANCH="development"
 
-pushd /opt/code/github/zero-os/0-robot
+mkdir -p /opt/code/github/zero-os
+pushd /opt/code/github/zero-os
+
+# cloning source code
+git clone --depth=1 -b ${BRANCH} https://github.com/zero-os/0-robot
+pushd 0-robot
 pip3 install .
 popd
