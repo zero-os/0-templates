a98ca72730ab6d1f2ff0eda141eb3c636a767405
[Dinaamagdy] <[magdyd@greenitglobe.com]>
2018-06-07 13:30:31 +0200

    edit

--- tests/integration_tests/travis/travis_script.sh
+++ tests/integration_tests/travis/travis_script.sh
@@ -37,4 +37,4 @@ cpu_zt_ip=$(cat /tmp/cpu_zt_ip.txt)
 zrobot robot connect main http://${cpu_zt_ip}:6600; sleep 20
 
 echo "[+] Running tests ..."
-nosetests -v -s ${tests_path} --tc-file=config.ini --tc=main.redisaddr:${cpu_zt_ip}
+nosetests -v -s ${tests_path} --tc-file=config.ini --tc=main.redisaddr:${cpu_zt_ip} --tc=main.secert:${itsyouonline_secert} --tc=main.app_id:${itsyouonline_app_id}
