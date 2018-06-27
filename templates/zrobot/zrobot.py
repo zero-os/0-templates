100190c6d2166e032db2c91c313cb1ba45c1fefc
Chris Van de Gejuchte <chris.vandegejuchte@gmail.com>
2018-06-27 16:25:05 +0200

    adds flist arg to zrobot upgrade

--- templates/zrobot/zrobot.py
+++ templates/zrobot/zrobot.py
@@ -160,11 +160,17 @@ class Zrobot(TemplateBase):
         self.state.delete('actions', 'start')
         self.state.delete('status', 'running')
 
-    def upgrade(self):
+    def upgrade(self, flist=None):
         """
         upgrade the container with an updated flist
         this is done by stopping the container and respawn again with the updated flist
+        
+        Keyword Arguments:
+            flist str -- flist to upgrade to if provided. (default: {None})
         """
+        if flist:
+            self.data['flist'] = flist
+
         # stop the robot process
         self.stop()
 
